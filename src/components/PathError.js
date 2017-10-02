b13d236ffb3cba8ea60b2aa2b870451e6e71c6fa
Kelly Ng <kellyng87@gmail.com>
2017-10-01 18:48:32 -0700

    add a better message to 404 error

--- src/components/PathError.js
+++ src/components/PathError.js
@@ -4,8 +4,15 @@ class PathError extends Component {
 
   render() {
     return(
-      <div>
-        Error 404 - Path not found
+      <div className='row'>
+        <div className='col-md-12'>
+          <h2>
+            HTTP 404 Error - not found!
+          </h2>
+        </div>
+        <div className='col-md-12'>
+          Something a little weird happened... we're really sorry about that!  It looks like you tried to find a post that someone deleted!
+        </div>
       </div>
     )
   }
